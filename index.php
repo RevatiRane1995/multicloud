b34b9f2b53ca1842c331d9f2a5002df36c83df3c
RevatiRane1995 <66860282+RevatiRane1995@users.noreply.github.com>
2020-06-13 15:52:10 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <h1>WELCOME TO THE WORLD OF MULTI CLOUD</h1><<br/>
 <pre>
-      <img src="" height=500 width=500>
+      <img src="Lighthouse.jpg" height=500 width=500>
       <?php
           print `ifconfig`;
        ?>
